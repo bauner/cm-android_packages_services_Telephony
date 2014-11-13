a52ae58579eb51ff598a1eebffb1e8c500e97b6e
Danny Baumann <dannybaumann@web.de>
2014-11-12 17:32:37 -0800

    Move message blacklist to framework (2/3).
    Change-Id: Ie1bea95513b985e79da2e28c86b15a2042c98754

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -1923,7 +1923,6 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         // Look up the voicemail ringtone name asynchronously and update its preference.
         new Thread(mVoicemailRingtoneLookupRunnable).start();
-        lookupRingtoneName();
         updateBlacklistSummary();
     }
 
